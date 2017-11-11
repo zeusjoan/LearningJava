45d3dea7e4a1537048d7d456b23f066f1bcaf16c
Pawel Krolicki <pawel.krolicki@hotmail.com>
2017-11-11 01:17:58 +0100

    Next day learning

--- Beginning/Chapter1/PiosenkaOPiwie.java
+++ Beginning/Chapter1/PiosenkaOPiwie.java
@@ -17,14 +17,16 @@ class PiosenkaOPiwie{
 
 				iloscButelek = iloscButelek -1;
 
-			if (iloscButelek > 0){
+			if (iloscButelek == 0){
 		
-			  System.out.println(iloscButelek + " " + slowo + " of beer on the wall");
+			  
+			System.out.println(iloscButelek + " " + slowo + " of beer on the wall");
+			System.out.println("No more bottles of beer on the wall");			
 			}
-			else {
-           			   System.out.println("No more bottles of beer on the wall");			
+		//	else {
+           	//		   System.out.println("No more bottles of beer on the wall");			
 		
-			}// koniec else
+		//	}// koniec else
 		}// koniec while
 	}// koniec metody main
 } //koniec klasy
---
+++ Beginning/Chapter1/ZagadkowyBasen1.java
@@ -0,0 +1,24 @@
+class ZagadkowyBasen1 {
+	public static void main(String[] args) {
+		int x = 0;
+		while (        ) {
+			
+			if ( x < 1) {
+				
+				
+			}
+			if (      ) {
+				
+				
+			}
+			if ( x == 1 ){
+				
+			}
+			if (      ) {
+		
+			}
+			System.out.println("");
+
+		}
+	}
+}
