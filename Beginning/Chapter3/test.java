1109c59009878c395a38ec99b11c5941b929f79c
Pawel Krolicki <pawel.krolicki@wipro.com>
2017-11-17 12:34:14 +0100

    Next

---
+++ Beginning/Chapter3/test.java
@@ -0,0 +1,15 @@
+class test {
+    
+    public static void main (String[] args) {
+        
+	int a = 1;
+	int b = 1;
+	int c = b++;
+
+	    
+	System.out.println("To jest zmienna a:" + a);
+	System.out.println("To jest zmienna b:" + b);
+	System.out.println("To jest zmienna c:" + c);
+    }
+}
+		
