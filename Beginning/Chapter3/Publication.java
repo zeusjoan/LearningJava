1efde0b6e0459ca06f8c40defffee514719aaabd
Pawel Krolicki <pawel.krolicki@wipro.com>
2017-11-17 14:43:09 +0100

    OK

---
+++ Beginning/Chapter3/Publication.java
@@ -0,0 +1,26 @@
+public class Publication {
+	private String title;
+	private String publisher;
+	private int year;
+	private String ident;
+	private double prince;
+	private int quantity;
+
+	public Publication(String t, String pb, int y,
+			   String i, double pr, int q)
+
+	{
+		title = t;
+		publisher = pb;
+		yesr = y;
+		ident = i;
+		prince = pr;
+		quantity =q;
+
+	}
+
+
+
+
+
+}
